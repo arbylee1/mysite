764f025bdb811d50662055ae94ce984549a2f020
Albert Li <arbylee@gmail.com>
2017-10-15 20:40:46 -0400

    added war building

--- src/test/java/mysite/model/api/movie/MovieConnectorTest.java
+++ src/test/java/mysite/model/api/movie/MovieConnectorTest.java
@@ -1,6 +1,8 @@
 package mysite.model.api.movie;
 import org.junit.Test;
 
+import java.sql.SQLException;
+
 public class MovieConnectorTest {
     MovieConnector connector = MovieConnector.getConnector();
 
@@ -8,6 +10,10 @@ public class MovieConnectorTest {
     public void testRegister() {
         System.out.println("Testing user registration");
         System.out.println("Testing add user \"arbylee\" with password \"1234\"");
-        connector.registerUser("arbylee", "1234");
+        try {
+            connector.registerUser("arbylee", "1234");
+        } catch (SQLException e) {
+
+        }
     }
 }
